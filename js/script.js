c23b302182ffec12f6baf21e52f239f3d7cbced5
Francine Eli Barbosa <francine.eli@gmail.com>
2023-02-07 18:05:01 -0300

    restructing

---
+++ js/script.js
@@ -0,0 +1,5 @@
+if(document.getElementById("username").value != "" && document.getElementById("usermail").value != ""){
+    alert("Pronto! Você receberá as novidades por email!")
+  }else{
+    alert("Ops! Faltou preencher algum campo. Verifique e tente novamente!")
+  }
